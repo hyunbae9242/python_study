ac2b3e31f11d6af5f83bbd361029b7bb641576ae
hyunbae <hyunbae9242@gmail.com>
2023-08-05 17:25:58 +0900

    init

---
+++ OneDay.py
@@ -0,0 +1,17 @@
+a = "python"
+b = "is"
+c = "fun"
+
+print(a + " " + b + " " + c)
+print(a * 3)
+
+str1 = "Life is too short"
+
+print(str1[3])
+
+startIndex = 1
+getCount = 100
+
+print(str1[startIndex: startIndex+getCount])
+
+print(3+5)
